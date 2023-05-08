4fbd3b43c5db5f843349c3e1bdb4a44b082534f5
rpd20939 <132942662+rpd20939@users.noreply.github.com>
2023-05-08 14:52:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 print(streamlit.title('My Parents New Healthy Diner'))
+
+print(streamlit.header('Breakfast Menu'))
+print(streamlit.text('Omega3 & Blueberry Oatmeal'))
+print(streamlit.text('Kale, Spinach & Rocket Smoothie'))
+print(streamlit.text('Hard-Boiled Free-Range Egg'))
