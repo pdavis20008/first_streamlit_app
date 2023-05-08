358fbd0837bc8d2284de126c3f613e028af69193
rpd20939 <132942662+rpd20939@users.noreply.github.com>
2023-05-08 14:53:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-print(streamlit.title('My Parents New Healthy Diner'))
+streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
